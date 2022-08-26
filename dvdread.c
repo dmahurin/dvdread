fabaac6e6d333837ec10bb300e26d554c672b2e4
Don Mahurin <@>
2022-08-26 16:39:02 -0700

    On read error, skip to next VOBU

--- dvdread.c
+++ dvdread.c
@@ -20,6 +20,7 @@
 #include <stdarg.h>
 #include <dvdread/dvd_reader.h>
 #include <dvdread/dvd_udf.h>
+#include <dvdread/ifo_read.h>
 #include <dvdcss/dvdcss.h>
 
 static int dumpsector(unsigned char *);
@@ -37,6 +38,10 @@ struct vts_file_extent_s {
     int type;
     unsigned int start;
     unsigned int end;
+
+    unsigned int vtsn;
+    unsigned int vpart;
+    unsigned int vstart; // start of title vts or menu vts
 };
 typedef struct vts_file_extent_s vts_file_extent_t;
 #define MAXTITLES 100
@@ -54,8 +59,9 @@ static int findfile(int pos) {
 }
 static int addfile(char *fname, int start, int len) {
     char *bname, *ext;
-    int type, title, part;
+    int type, vtsn, vpart;
     int end = start + (len + DVD_VIDEO_LB_LEN - 1) / DVD_VIDEO_LB_LEN;
+    int vstart;
     if (fname == NULL) return 0;
     if (nfiles >= MAXVFILES) {
         fprintf(stderr, "can't add %s [%u -- %u)\n", fname, start, end);
@@ -75,10 +81,22 @@ static int addfile(char *fname, int start, int len) {
     } else {
         type = 0;
     };
+    vtsn = 0;
+    vpart = 0;
+    vstart = start;
     if (type != 0) {
         if (0 == strncasecmp(bname, "VIDEO_TS", ext - bname)) {
-        } else if (2 == sscanf(bname, "VTS_%d_%d", &title, &part)) {
+        } else if (2 == sscanf(bname, "VTS_%d_%d", &vtsn, &vpart)) {
             type += 3;
+            if(vpart > 1) {
+                int n = nfiles;
+                while(--n > 0) {
+                    if(file[n].vtsn == vtsn && file[n].vpart == 1) {
+                        file[nfiles].vstart = file[n].start;
+                        break;
+                    }
+                }
+            }
         } else {
             type = 0;
         };
@@ -95,6 +113,8 @@ static int addfile(char *fname, int start, int len) {
     strncpy(file[nfiles].fname, fname, sizeof(file[nfiles].fname));
     file[nfiles].fname[sizeof(file[nfiles].fname) - 1] = '\0';
     file[nfiles].start = start;
+    file[nfiles].vstart = vstart;
+    file[nfiles].vtsn = vtsn;
     file[nfiles++].end = end;
     return 1;
 }
@@ -125,12 +145,33 @@ int ss_fprintf(unsigned *ss, unsigned s, vts_file_extent_t *file,
     return nprinted;
 }
 
+int find_next_vobu(dvd_reader_t *dvd, int vtsn, int vpart, int vstart, int s) {
+    s -= vstart;
+
+    ifo_handle_t *vts_ifo = ifoOpen(dvd, vtsn);
+
+    vobu_admap_t *vobu_admap = vpart == 0 ? vts_ifo->menu_vobu_admap : vts_ifo->vts_vobu_admap;
+    int n = (vobu_admap->last_byte + 1 + - VOBU_ADMAP_SIZE) / 4;
+
+    for (int i = 0; i < n; i++) {
+        if(s < vobu_admap->vobu_start_sectors[i]) {
+		s = vobu_admap->vobu_start_sectors[i];
+		break;
+        }
+    }
+
+    ifoClose(vts_ifo);
+
+    return s + vstart;
+}
+
 int main(int argc, char *argv[]) {
     dvd_reader_t *prdr = NULL;
     dvdcss_t dvdcss;
     unsigned char p_data[DVDCSS_BLOCK_SIZE * 2];
     unsigned char *p_buffer;
     unsigned int  s, ss, s1, s2;
+    unsigned int skip_to_s = 0;
     char fname[23];
     uint32_t start, len;
     int t, v, lastfile, curfile, r, stripreg;
@@ -166,7 +207,7 @@ int main(int argc, char *argv[]) {
             snprintf(fname, sizeof(fname),
                 "/VIDEO_TS/VTS_%02d_%d.VOB", t, v);
             start = UDFFindFile(prdr, fname, &len);
-            if (!start || !len) break;
+            if (!start || !len) continue;
             addfile(fname, start, len);
         }
         if (t != 0 && v == 0 && (!start || !len)) break;
@@ -174,7 +215,7 @@ int main(int argc, char *argv[]) {
         start = UDFFindFile(prdr, fname, &len);
         if (start && len) addfile(fname, start, len);
     }
-    DVDClose(prdr);
+
     /* Initialize libdvdcss */
     dvdcss = dvdcss_open(argv[1]);
     if (dvdcss == NULL) {
@@ -199,7 +240,8 @@ int main(int argc, char *argv[]) {
         /* Print current sector/range/filename: */
         ss_fprintf(&ss, s, file, curfile, 0, stderr, "");
         /* Seek for VOB key if entering new VOB, skip otherwise: */
-        if (curfile >= 0 && (file[curfile].type == vmg_vob
+        if(skip_to_s > s) { r = s; }
+        else if (curfile >= 0 && (file[curfile].type == vmg_vob
                     || file[curfile].type == vts_vob)
                 && curfile != lastfile) {
             st = "seek key";
@@ -210,7 +252,10 @@ int main(int argc, char *argv[]) {
         };
         if (r != (int)s) goto CSSERR;
         /* Decrypt if inside VOB, read plain data otherwise: */
-        if (curfile >= 0 && (file[curfile].type == vmg_vob
+        if(skip_to_s > s) {
+            memset(p_buffer, '\0', DVDCSS_BLOCK_SIZE);
+            r = 1;
+        } else if (curfile >= 0 && (file[curfile].type == vmg_vob
                     || file[curfile].type == vts_vob)) {
             st = "decrypt";
             r = dvdcss_read(dvdcss, p_buffer, 1, DVDCSS_READ_DECRYPT);
@@ -223,8 +268,14 @@ int main(int argc, char *argv[]) {
             /* Print error warning: */
             ss_fprintf(&ss, s, file, curfile, 1, stderr,
                     "%s - %s\n", st, dvdcss_error(dvdcss));
+
             /* XXX: substitute zeroes for bad sector data: */
             memset(p_buffer, '\0', DVDCSS_BLOCK_SIZE);
+
+            if(file[curfile].type == vts_vob) {
+                skip_to_s = find_next_vobu(prdr, file[curfile].vtsn, file[curfile].vpart, file[curfile].vstart, s);
+                ss_fprintf(&ss, s, file, curfile, 1, stderr, "skiping to vobu at %d\n", skip_to_s);
+            }
         } else {
             /* Report/modify region restriction mask in the 1st sector
              * of VMG IFO and BUP files: */
@@ -254,14 +305,17 @@ int main(int argc, char *argv[]) {
 EOFDVD:
     fprintf(stderr, "end of file %s\n", argv[1]);
     dvdcss_close(dvdcss);
+    DVDClose(prdr);
     return 0;
 STDERR:
     perror(NULL);
     dvdcss_close(dvdcss);
+    DVDClose(prdr);
     return 1;
 CSSERR:
     fprintf(stderr, "%s - %s\n", st, dvdcss_error(dvdcss));
     dvdcss_close(dvdcss);
+    DVDClose(prdr);
     return 1;
 }
 
